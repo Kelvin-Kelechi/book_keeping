1f9876d5c7e634ba5c3f3257baf6c1ad8eccc72c
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-10 22:49:35 +0100

    still working on mailList

--- frontend/src/pages/list/List.jsx
+++ frontend/src/pages/list/List.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import {
+  ListContainer,
+  ListResult,
+  ListSearch,
+  ListWrapper,
+  SearchItem,
+  SearchTitle,
+} from "./list.style";
 
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <>
+      <Navbar />
+      <Header />
+      <ListContainer>
+        <ListWrapper>
+          <ListSearch>
+            <SearchTitle>Search</SearchTitle>
+            <SearchItem>
+              <label htmlFor="">Destination</label>
+              <input type="text" />
+            </SearchItem>
+            <SearchItem>
+              <label htmlFor="">Check-in date</label>
+               
+            </SearchItem>
+          </ListSearch>
+          <ListResult></ListResult>
+        </ListWrapper>
+      </ListContainer>
+    </>
+  );
+};
 
-export default List
+export default List;
---
+++ frontend/src/pages/list/list.style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+
+`
+export const ListWrapper = styled.div`
+
+`
+export const ListSearch = styled.div`
+
+`
+export const ListResult = styled.div`
+
+`
+export const SearchTitle = styled.div`
+
+`
+export const SearchItem = styled.div`
+
+`
