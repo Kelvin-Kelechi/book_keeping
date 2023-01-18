5ffeb59040c7188ad895c023545033bb89205e15
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-18 14:21:27 +0100

    break time

--- frontend/src/pages/hotels/Hotels.jsx
+++ frontend/src/pages/hotels/Hotels.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import MailList from "../../components/mailList/MailList";
 import {MdLocationOn} from 'react-icons/md'
-import { HotelContainer, HotelDistance, HotelImages, HotelImgWrapper, HotelPriceHighLight, HotelTitle, HotelWrapper } from "./hotels.style";
+import { HotelAddress, HotelContainer, HotelDesc, HotelDetails, HotelDetailsPrice, HotelDetailsTexts, HotelDistance, HotelImages, HotelImgWrapper, HotelPriceHighLight, HotelTitle, HotelWrapper } from "./hotels.style";
 const Hotels = () => {
   const photos = [
     {
@@ -41,13 +43,44 @@ const Hotels = () => {
             Book a stay over $114 at this property and get a free airport taxi
           </HotelPriceHighLight>
           <HotelImages>
-            {photos.map((photo, i)=>(
+            {photos.map((photo, i) => (
               <HotelImgWrapper key={i}>
                 <img src={photo.src} alt="" />
               </HotelImgWrapper>
             ))}
           </HotelImages>
+          <HotelDetails>
+            <HotelDetailsTexts>
+              <HotelTitle>Stay in the heart of City</HotelTitle>
+              <HotelDesc>
+                Located a 5-minute walk from St. Florian's Gate in Krakow, Tower
+                Street Apartments has accommodations with air conditioning and
+                free WiFi. The units come with hardwood floors and feature a
+                fully equipped kitchenette with a microwave, a flat-screen TV,
+                and a private bathroom with shower and a hairdryer. A fridge is
+                also offered, as well as an electric tea pot and a coffee
+                machine. Popular points of interest near the apartment include
+                Cloth Hall, Main Market Square and Town Hall Tower. The nearest
+                airport is John Paul II International Kraków–Balice, 16.1 km
+                from Tower Street Apartments, and the property offers a paid
+                airport shuttle service.
+              </HotelDesc>
+            </HotelDetailsTexts>
+            <HotelDetailsPrice>
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </HotelDetailsPrice>
+          </HotelDetails>
         </HotelWrapper>
+         <MailList/>
+         <Footer/>
       </HotelContainer>
     </>
   );
--- frontend/src/pages/hotels/hotels.style.js
+++ frontend/src/pages/hotels/hotels.style.js
@@ -1,26 +1,88 @@
 import styled from 'styled-components'
 
 export const HotelContainer = styled.div`
-
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`;
 export const HotelWrapper = styled.div`
-
-`
+  width: 100%;
+  max-width: 1024px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  position: relative;
+`;
 export const HotelTitle = styled.h1`
-
-`
+  font-size: 24px;
+`;
 export const HotelAddress = styled.div`
-
-`
-export const HotelDistance = styled.span`
-
-`
-export const HotelPriceHighLight = styled.span`
-
-`
-export const HotelImages = styled.div`
-
-`
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
 export const HotelImgWrapper = styled.div`
-
-`
+  width: 33%;
+  img {
+    width: 100%;
+    object-fit: cover;
+    cursor: pointer;
+  }
+`;
+export const HotelDistance = styled.span`
+  color: #0071c2;
+  font-weight: 500;
+  
+`;
+export const HotelPriceHighLight = styled.span`
+  color: #008009;
+  font-weight: 500;
+`;
+export const HotelImages = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+export const HotelDetails = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  margin-top: 20px;
+`;
+export const HotelDetailsTexts = styled.div`
+  flex: 3;
+`;
+export const HotelDetailsPrice = styled.div`
+  flex: 1;
+  background-color: #ebf3ff;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  h1 {
+    font-size: 18px;
+    color: #555;
+  }
+  h2 {
+    font-weight: 300;
+  }
+  span {
+    font-size: 14px;
+  }
+  button {
+    border: none;
+    padding: 10px 20px;
+    background-color: #0071c2;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+`;
+export const HotelDesc = styled.p`
+  font-size: 14px;
+  margin-top: 20px;
+`;
+ 
