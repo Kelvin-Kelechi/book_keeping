e3fb293ff4ecc4a33a5785afa2a9ba2bfde960b0
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-20 15:50:17 +0100

    working routes/auth

---
+++ backend/routes/auth.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express()
+
+router.get('/',(req, res)=>{
+    res.json('csgv dbvefrbfhrbfh rjvnrjbgjfrb')
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from './routes/auth.js'
 const app = express();
 dotenv.config();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => { 
-  app.listen(process.env.PORT, () => {
-    console.log("connected datebase and listening to port", process.env.PORT);
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected datebase and listening to port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
   });
-})
-.catch((error)=>{
-    console.log(error)
-})
+
+  app.use('/auth',authRoute)
+
+  
