07d3e05c5afaa10f5b6825111c8d5c5c7f2824c2
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-21 00:41:28 +0100

    setting up models

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoute from './routes/auth.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+import usersRoute from './routes/users.js'
+
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -22,5 +26,13 @@ mongoose
   });
 
   app.use('/auth',authRoute)
+  app.use('/hotels',hotelsRoute)
+  app.use('/users',usersRoute)
+  app.use('/rooms',roomsRoute)
 
+
+
+
+  
+  
   
