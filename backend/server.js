300ec3d388f8ffb92507ad491e6a5c36e3397e90
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-20 14:05:00 +0100

    finished setting up server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+const app = express();
+dotenv.config();
 
-const app = express()
-
-app.listen(8000, ()=>{
-    console.log('connected to backend!')
+mongoose.connect(process.env.MONGO_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => { 
+  app.listen(process.env.PORT, () => {
+    console.log("connected datebase and listening to port", process.env.PORT);
+  });
+})
+.catch((error)=>{
+    console.log(error)
 })
