e73a88fb155371aec230b461a9207db9c9ff4729
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-23 16:19:47 +0100

    adding post, put,delete,get,and getall

--- backend/controllers/hotelsController.js
+++ backend/controllers/hotelsController.js
@@ -1,13 +1,49 @@
- import Hotel from "../models/Hotel.js";
+import Hotel from "../models/Hotel.js";
 
-export const HOTELS_GET = async (req,res) => {
+export const HOTELS_POST = async (req, res) => {
   const newHotel = new Hotel(req.body);
 
   try {
-    const savedHotel = await newHotel.save()
-    res.status(200).json(savedHotel)
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
   } catch (error) {
-    res.status(500).json(error); 
+    res.status(500).json(error);
   }
 };
-  
+
+export const HOTELS_PUT = async (req, res) => {
+  try {
+    const updateHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updateHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+export const HOTELS_DELETE = async (req, res) => {
+  try {
+     await  Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("hotel has been deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+export const HOTELS_GET = async (req, res) => {
+  try {
+    const getHotel = await  Hotel.findById(req.params.id);
+    res.status(200).json(getHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+export const HOTELS_GET_ALL = async (req, res) => {
+  try {
+    const getallHotel = await  Hotel.find();
+    res.status(200).json(getallHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { HOTELS_GET } from "../controllers/hotelsController.js";
+import { HOTELS_POST,HOTELS_PUT,HOTELS_DELETE,HOTELS_GET_ALL,HOTELS_GET } from "../controllers/hotelsController.js";
 const router = express();
 
-router.post("/", HOTELS_GET);
+router.post("/", HOTELS_POST);
+router.put("/:id", HOTELS_PUT); 
+router.delete("/:id", HOTELS_DELETE);
+router.get("/:id", HOTELS_GET);
+router.get("/", HOTELS_GET_ALL);
  
 export default router;
