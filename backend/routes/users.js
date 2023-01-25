fb8774f6e273f7e9aa621d8872c426968dd7a7c3
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-25 23:16:53 +0100

    made change

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { USER_DELETE, USER_GET, USER_GET_ALL, USER_PUT } from "../controllers/userController.js";
+import {
+  USER_DELETE,
+  USER_GET,
+  USER_GET_ALL,
+  USER_PUT,
+} from "../controllers/userController.js";
+import { VerifyToken, VerifyUser } from "../utils/verifyToken.js";
 
-const router = express();
- 
+const router = express.Router();
+
+router.get("/checkauthen", VerifyToken, (req, res, next) => {
+  res.send("Hello User, you are logged in");
+});
+router.get("/checkuser/:id", VerifyUser, (req, res, next) => {
+  res.send("Hello User, you are logged in and you can delete your acc");
+});
 router.put("/:id", USER_PUT);
-router.delete("/:id", USER_DELETE);
+router.delete("/:id", USER_DELETE);    
 router.get("/:id", USER_GET);
 router.get("/", USER_GET_ALL);
 
---
+++ backend/utils/verifyToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { CreateError } from "./error.js";
+
+export const VerifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) return next(CreateError(401, "You are not authenticated"));
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(CreateError(403, "token not valid"));
+    req.user = user;
+    next();
+  });
+};
+export const VerifyUser = (req, res, next) => {
+  VerifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(CreateError(403, "You are not authorized"));
+    }
+  });
+};
