830dab4acdaec95f7f95c08b98a730bcf0124e3a
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-01-28 00:03:31 +0100

    okay

--- backend/controllers/hotelsController.js
+++ backend/controllers/hotelsController.js
@@ -47,3 +47,14 @@ export const HOTELS_GET_ALL = async (req, res, next) => {
     next(error)
   }
 };
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(',')
+  try {
+    const list = await  Promise.all(cities.map((city)=>{
+      return Hotel.countDocuments({city:city})
+    }))
+    res.status(200).json(list);
+  } catch (error) {
+    next(error)
+  }
+};
--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,12 +1,21 @@
 import express from "express";
-import { HOTELS_POST,HOTELS_PUT,HOTELS_DELETE,HOTELS_GET_ALL,HOTELS_GET } from "../controllers/hotelsController.js";
+import {
+  HOTELS_POST,
+  HOTELS_PUT,
+  HOTELS_DELETE,
+  HOTELS_GET_ALL,
+  HOTELS_GET,
+  countByCity,
+} from "../controllers/hotelsController.js";
 import { VerifyAdmin } from "../utils/verifyToken.js";
 const router = express();
 
 router.post("/", VerifyAdmin, HOTELS_POST);
-router.put("/:id", VerifyAdmin, HOTELS_PUT); 
+router.put("/:id", VerifyAdmin, HOTELS_PUT);
 router.delete("/:id", VerifyAdmin, HOTELS_DELETE);
-router.get("/:id", HOTELS_GET);
+router.get("/find/:id", HOTELS_GET);
 router.get("/", HOTELS_GET_ALL);
- 
+router.get("/countByCity", countByCity);
+router.get("/countByType", HOTELS_GET_ALL);
+
 export default router;
