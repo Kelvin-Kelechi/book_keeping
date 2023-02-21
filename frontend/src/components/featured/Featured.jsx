0904619ed5119a8805355aa0cf327d581d16b45c
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-02-21 20:46:03 +0100

    i am going to resume work today

--- frontend/src/components/featured/Featured.jsx
+++ frontend/src/components/featured/Featured.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-import useFetch from "../../hooks/useFetch"; 
+import useFetch from "../../hooks/useFetch";
 import { Feature, FeatureItem, FeatureTitle, Img } from "./featured.style";
- /FeatureTitle>
+
+const Featured = () => {
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=lagos,abuja,anambara"
+  );
+  return (
+    <Feature>
+      {loading ? (
+        "Please...wait"
+      ) : (
+        <>
+          <FeatureItem>
+            <Img
+              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              alt=""
+            />
+            <FeatureTitle>
+              <h1>Lagos</h1>
+              <h2>{data[0]} achivements</h2>
+            </FeatureTitle>
           </FeatureItem>
+
           <FeatureItem>
             <Img
               src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,12 @@ import { SearchContextProvider } from './context/context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
+  <React.StrictMode>
+    <SearchContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SearchContextProvider>
+  </React.StrictMode>
 );
  
