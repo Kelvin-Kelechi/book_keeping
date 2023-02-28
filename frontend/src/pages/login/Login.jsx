470c6b1bb386f128df67bd6545d9cebce648e284
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-02-28 23:02:15 +0100

    how are u

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -16,16 +16,20 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-        const res = await axios.get('/auth/login', credentials)
-        dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      const res = await axios.post("/auth/login", credentials);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (error) {
-         dispatch({ type: "LOGIN_FAILURE", payload: error.res.data });
+      dispatch({ type: "LOGIN_FAILURE", payload: error.response.data });
     }
   };
-  console.log(user)
+  console.log(user);
   return (
     <LoginContainer>
       <Lcontainer>
+        <div>
+          {" "}
+          <h1>Login</h1>
+        </div>
         <input
           type="text"
           placeholder="Enter username"
@@ -44,6 +48,11 @@ const Login = () => {
           Login
         </button>
         {error && <span>{error.message}</span>}
+        <div>
+          <p>
+            Already have an account? <strong>Create an account</strong>{" "}
+          </p>
+        </div>
       </Lcontainer>
     </LoginContainer>
   );
