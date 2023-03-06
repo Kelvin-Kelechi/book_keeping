9c9fea9bbfcb1f0472100e3ee9bd683b6dac4c87
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-03-06 13:52:41 +0100

    ok next

--- frontend/src/components/reserve/Reserve.jsx
+++ frontend/src/components/reserve/Reserve.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaTimes } from "react-icons/fa";
-import { Container, ReserveContainer } from "./reserve.style";
+import { SearchContext } from "../../context/context";
+import { Container, Rdesc, ReserveContainer, Ritem, RitemInfo, Rmax, Rtitle } from "./reserve.style";
+import useFetch from "../../hooks/useFetch";
 const Reserve = ({ setOpenModal, hotelId }) => {
+  const { data, error, loading } = useFetch(`/hotels/room/${hotelId}`);
   return (
     <Container>
       <ReserveContainer>
         {" "}
         <FaTimes onClick={() => setOpenModal(false)} />{" "}
+        <span>Select your rooms:</span>
+      {data.map((item) => (  
+          <Ritem>
+            <RitemInfo>
+              <Rtitle>{item.title}</Rtitle> 
+              <Rdesc>{item.desc}</Rdesc>
+              <Rmax>
+                Max people: <b>{item.maxPeople}</b>
+              </Rmax>
+            </RitemInfo>
+          </Ritem>
+        ))}
       </ReserveContainer>
     </Container>
   );
--- frontend/src/components/reserve/reserve.style.js
+++ frontend/src/components/reserve/reserve.style.js
@@ -2,3 +2,9 @@ import styled from 'styled-components'
 
 export const Container = styled.div``
 export const ReserveContainer = styled.div``
+export const Ritem = styled.div``
+export const RitemInfo = styled.div``
+export const Rtitle = styled.div``
+export const Rdesc = styled.div``
+export const Rmax = styled.div``
+ 
