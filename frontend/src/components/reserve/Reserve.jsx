077d93db7ab48cdbe43b52082097df3828905cf9
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-03-07 19:26:22 +0100

    are u ok

--- frontend/src/components/reserve/Reserve.jsx
+++ frontend/src/components/reserve/Reserve.jsx
@@ -1,25 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FaTimes } from "react-icons/fa";
-import { SearchContext } from "../../context/context";
-import { Container, Rdesc, ReserveContainer, Ritem, RitemInfo, Rmax, Rtitle } from "./reserve.style";
+import {
+  Container,
+  Rdesc,
+  ReserveContainer,
+  Ritem,
+  RitemInfo,
+  Rmax,
+  Room,
+  Rprice,
+  Rtitle,
+} from "./reserve.style";
 import useFetch from "../../hooks/useFetch";
 const Reserve = ({ setOpenModal, hotelId }) => {
   const { data, error, loading } = useFetch(`/hotels/room/${hotelId}`);
+  const [check, setCheck] = useState([]);
+  const handleCheck = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    setCheck(
+      checked ? [...check, value] : check.filter((item) => item !== value)
+    );
+  };
+
+  console.log(check);
   return (
     <Container>
       <ReserveContainer>
         {" "}
         <FaTimes onClick={() => setOpenModal(false)} />{" "}
         <span>Select your rooms:</span>
-      {data.map((item) => (  
-          <Ritem>
+        {data.map((item) => (
+          <Ritem key={item._id}>
             <RitemInfo>
-              <Rtitle>{item.title}</Rtitle> 
+              <Rtitle>{item.title}</Rtitle>
               <Rdesc>{item.desc}</Rdesc>
               <Rmax>
                 Max people: <b>{item.maxPeople}</b>
               </Rmax>
+              <Rprice>{item.price}</Rprice>
             </RitemInfo>
+            {item.roomNumbers.map((roomNumber) => (
+              <Room key={roomNumber._id}>
+                <span>{roomNumber.number}</span>
+                <input
+                  type="checkbox"
+                  value={roomNumber._id}
+                  onClick={handleCheck}
+                />
+              </Room>
+            ))}
           </Ritem>
         ))}
       </ReserveContainer>
--- frontend/src/components/reserve/reserve.style.js
+++ frontend/src/components/reserve/reserve.style.js
@@ -6,5 +6,6 @@ export const Ritem = styled.div``
 export const RitemInfo = styled.div``
 export const Rtitle = styled.div``
 export const Rdesc = styled.div``
-export const Rmax = styled.div``
+export const Rmax = styled.div`` 
 export const Rprice = styled.div``
+export const Room = styled.div``
