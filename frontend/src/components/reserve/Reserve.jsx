886a7c79e3567f1e036d63d4aa0bafcf95f1092c
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-03-08 23:47:49 +0100

    i will be okay

--- frontend/src/components/reserve/Reserve.jsx
+++ frontend/src/components/reserve/Reserve.jsx
@@ -12,9 +12,11 @@ import {
   Rtitle,
 } from "./reserve.style";
 import useFetch from "../../hooks/useFetch";
+import { SearchContext } from "../../context/context";
 const Reserve = ({ setOpenModal, hotelId }) => {
   const { data, error, loading } = useFetch(`/hotels/room/${hotelId}`);
   const [check, setCheck] = useState([]);
+  const { dates } = useContext(SearchContext);
   const handleCheck = (e) => {
     const checked = e.target.checked;
     const value = e.target.value;
@@ -22,8 +24,21 @@ const Reserve = ({ setOpenModal, hotelId }) => {
       checked ? [...check, value] : check.filter((item) => item !== value)
     );
   };
+  const getDate = (startDate, endDate) => {
+    const start = new Date(startDate)
+    const end = new Date(endDate)
+    const date = new Date(start.getTime());
+    let list = [];-0i0
+    while (data <= end) {
+      list.push(new Date(date));
+      date.setDate(date.getTime() + 1);
+    }
+    return list;
+  };
+  console.log(getDate(dates[0]?.startDate, dates[0]?.endDate));
+  const handleClick = ()=>{
 
-  console.log(check);
+  }
   return (
     <Container>
       <ReserveContainer>
@@ -53,6 +68,7 @@ const Reserve = ({ setOpenModal, hotelId }) => {
           </Ritem>
         ))}
       </ReserveContainer>
+      <button onClick={handleClick}>Reserve now</button>
     </Container>
   );
 };
