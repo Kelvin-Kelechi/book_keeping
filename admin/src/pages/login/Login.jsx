0121e68ec21cdcff835a444cffa7ada304bea1ad
Kelvin-Kelech <ogkelvin99@gmail.com>
2023-04-02 23:44:20 +0100

    added login page

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,9 +1,71 @@
-import "./login.scss"
-
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import { LoginContainer, Lcontainer } from "./login.style";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+  const { error, loading, dispatch, user } = useContext(AuthContext);
 
-export default Login
+  const handleOnchange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    navigate("/");
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE", payload: error.response.data });
+    }
+  };
+  console.log(user);
+  return (
+    <LoginContainer>
+      <h2>Login</h2>
+      <form action="" method="post">
+        <label htmlFor="">
+          <b>Username</b>
+        </label>
+        <input
+          type="text"
+          placeholder="Enter username"
+          name=""
+          id="username"
+          onChange={handleOnchange}
+        />
+        <br />
+        <br />
+        <label htmlFor="">
+          <b>Password</b>
+        </label>
+        <input
+          type="password"
+          placeholder="Enter password"
+          name=""
+          id="password"
+          onChange={handleOnchange}
+        />
+        <br />
+        <br />
+        <button disabled={loading} onClick={handleLogin} type="submit">
+          <b>Login</b>
+        </button>
+        {error && <span>{error.message}</span>}
+        <div>
+          <p>
+            Already have an account? <strong>Create an account</strong>{" "}
+          </p>
+        </div>
+      </form>
+    </LoginContainer>
+  );
+};
+
+export default Login;
